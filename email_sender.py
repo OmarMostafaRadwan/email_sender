69acef4437ce2c576021608ef61c2034c04ec438
OmarMostafaRadwan <137636180+OmarMostafaRadwan@users.noreply.github.com>
2023-07-04 21:28:46 +0300

    Add files via upload

---
+++ email_sender.py
@@ -0,0 +1,28 @@
+from email.message import EmailMessage 
+import ssl
+import smtplib
+
+
+email_sender = "omar123456@gmail.com"
+
+email_password = "1243"
+
+email_reciever = "yovitem847@dotvilla.com"
+
+subject = "Test"
+
+body = "hiiii"
+em = EmailMessage()
+
+em["From"]=email_sender
+em["To"]=email_reciever
+em["Subject"]=subject
+
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com",465,context=context) as smtp:
+    smtp.login(email_sender,email_password)
+    smtp.sendmail(email_sender,email_reciever,em.as_string())
+
